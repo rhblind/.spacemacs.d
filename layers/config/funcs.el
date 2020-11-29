703d7f3ea1349ee94fd807b416ecc8f96afa6eb4
Rolf Håvard Blindheim <rhblind@gmail.com>
2020-11-29 20:25:25 +0100

    Clean: Delete unused functions

--- layers/config/funcs.el
+++ layers/config/funcs.el
@@ -172,24 +172,3 @@ https://stackoverflow.com/a/60826269"
   (let ((oldpos (point)))
     (end-of-line)
     (newline-and-indent)))
-
-;;;; Timestamp to *Messages* buffer
-(defun current-time-microseconds ()
-  "Return the current time formatted to include microseconds."
-  (let* ((nowtime (current-time))
-         (now-ms (nth 2 nowtime)))
-    (concat (format-time-string "[%Y-%m-%dT%T" nowtime) (format ".%d]" now-ms))))
-
-(defun ad-timestamp-message (FORMAT-STRING &rest args)
-  "Advice to run before `message' that prepends a timestamp to each message.
-Activate this advice with:
-(advice-add 'message :before 'sh/ad-timestamp-message)"
-
-  (unless (string-equal FORMAT-STRING "%s%s")
-    (let ((deactivate-mark nil)
-          (inhibit-read-only t))
-      (with-current-buffer "*Messages*"
-        (goto-char (point-max))
-        (if (not (bolp))
-            (newline))
-        (insert (current-time-microseconds) " ")))))
