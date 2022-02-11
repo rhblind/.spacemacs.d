457ce3d0f113c836c5b03a12add2b733ad09208d
Rolf Håvard Blindheim <rolf.havard.blindheim@intility.no>
2022-02-11 01:01:37 +0100

    Add: Moved layers and packages definitions into spacemacs.org

--- layers/personal/packages.el
+++
@@ -1,9 +0,0 @@
-;;; Personal Layer -*- lexical-binding: t; -*-
-
-(setq personal-packages
-      '((outline-ivy :location local)
-        ))
-
-;;; Outline-ivy
-
-(defun personal/init-outline-ivy ())
